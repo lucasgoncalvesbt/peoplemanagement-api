5d5cc531f489119c64a7d878516c9b23b0e915c1
Lucas Torquato <front.end.lb@gmail.com>
2021-08-06 17:05:17 -0300

    starting repository

---
+++ src/main/java/one/digitalinnovation/peoplemanagementapi/PeoplemanagementapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.peoplemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplemanagementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplemanagementapiApplication.class, args);
+	}
+
+}
